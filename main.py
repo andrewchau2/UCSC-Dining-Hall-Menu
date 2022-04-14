911fc8c5bc79de6754f504ff91a29986b033893b
Andrew Chau <xfriendsx1@gmail.com>
2022-04-14 14:47:44 -0700

    Finished webscrapping food names and storing into JSON
    All requirements for Sprint 1 are finished. Sample JSON files are provided

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from selenium import webdriver
-# from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 import time
 import json
-# from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
 
 # Setup for web scrap. Path needs to change for chromedriver.exe once uploaded in the cloud.
@@ -17,15 +14,15 @@ driver = webdriver.Chrome(service=service)
 
 # Starting website
 driver.get("https://nutrition.sa.ucsc.edu/")
-#driver.get("https://nutrition.sa.ucsc.edu/label.aspx?locationNum=40&locationName=College+Nine%2fJohn+R.+Lewis+Dining+Hall&dtdate=04%2f11%2f2022&RecNumAndPort=061003*1")
+
+# Testing for sprint 2
+# driver.get("https://nutrition.sa.ucsc.edu/label.aspx?locationNum=40&locationName=College+Nine%2fJohn+R.+Lewis+Dining+Hall&dtdate=04%2f11%2f2022&RecNumAndPort=061003*1")
 
 
 # Starting from the main dining hall page, the driver moves to the nutrition page for all the food of that dining hall
-# driver.current_url MUST BE https://nutrition.sa.ucsc.edu/
 def moveToNutritionPage(dining_hall):
-    # dining_location = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CLASS_NAME, "locations"))
-    # print(dining_location.text)
-    driver.get("https://nutrition.sa.ucsc.edu/")
+
+    driver.get("https://nutrition.sa.ucsc.edu/") #Resets the page incase url is different
 
     dining_hall_link = WebDriverWait(driver, timeout=3).until(
         lambda d: d.find_element(By.LINK_TEXT, dining_hall))
@@ -39,6 +36,7 @@ def moveToNutritionPage(dining_hall):
 
     nutrition_page_link.click()
 
+
 # Given an list of all foods(Food genre and food names), parses it based on "--"
 # Returns an dictionary
 
@@ -48,22 +46,26 @@ def splitAllFoods(lst, dining_hall):
     current_food_type = None
     for i in lst:
         if i[0] == '-' and i[1] == '-':
+            removeDash = len(i) - 3
+            i = i[3:removeDash]
             current_food_type = i
             continue
 
         tmp = {
-            'food_type' : current_food_type,
-            'food_name' : i,
-            'dining_hall' : dining_hall
+            'food_type': current_food_type,
+            'food_name': i,
+            'dining_hall': dining_hall
         }
         newLst.append(tmp)
 
     return newLst
 
+# TODO: Sprint 2
 # Starting from the food item nutrition page, all nutrition facts are scrapped
 def singleNutritionFact(split_foods, count):
-    nutrition_facts_table = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.XPATH, '//table[@bordercolorlight="#000000"]'))
-    #print(nutrition_facts_table.text)
+    nutrition_facts_table = WebDriverWait(driver, timeout=3).until(
+        lambda d: d.find_element(By.XPATH, '//table[@bordercolorlight="#000000"]'))
+    # print(nutrition_facts_table.text)
     food_name = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CLASS_NAME, 'labelrecipe'))
     print(food_name.text)
 
@@ -71,7 +73,7 @@ def singleNutritionFact(split_foods, count):
     serving_size = arr[1]
     arr = arr[9:]
     arr.append(serving_size)
-    #arr.append(food_name.text)
+    # arr.append(food_name.text)
     for i in range(len(arr)):
         arr[i] = arr[i].lstrip()
 
@@ -95,25 +97,22 @@ def singleNutritionFact(split_foods, count):
     # split_foods[count]['iron'] = arr[17]
     # split_foods[count]['potassium'] = arr[18]
     # split_foods[count]['serving_size'] = arr[19]
-    #print(arr)
-
+    # print(arr)
 
 
 # Must call moveToNutritionPage() before calling this function.
+# Scraps all information(food genre, name, nutrition facts,etc) and writes it to a JSON file
 
 def getNutritionFacts(dining_hall):
     food_table = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.XPATH, '//table[@width="70%"]'))
-   # print(food_table.get_attribute("innerHTML"))
 
     inner_table = WebDriverWait(food_table, timeout=3).until(lambda d: d.find_elements(By.TAG_NAME, 'table'))
     size = len(inner_table) - 1;
-    # print(len(inner_table))
-    # print(inner_table[1].get_attribute("innerHTML"))
+
     all_foods = []
     for i in range(1, size, 2):
         try:
             getElement = inner_table[i].find_element(By.CLASS_NAME, "longmenucolmenucat")
-            #all_foods.append("--") #Placeholder to track start of each genre
             all_foods.append(getElement.text)
         except:
             getElement = inner_table[i].find_element(By.CLASS_NAME, "longmenucoldispname")
@@ -122,28 +121,46 @@ def getNutritionFacts(dining_hall):
     print(all_foods)
     split_foods = splitAllFoods(all_foods, dining_hall)
     print(split_foods)
+    convertToJSON(split_foods, dining_hall)
 
-    for i in range(len(split_foods)):
-        link = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.LINK_TEXT, split_foods[i]['food_name']))
-        link.click()
-        singleNutritionFact(split_foods, i)
-        driver.back()
-        # for j in range(1,len(split_foods[i])):
-        #     link = WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.LINK_TEXT, split_foods[i][j]))
-        #     link.click()
-        #     singleNutritionFact()
-        #     driver.back()
 
-    print(split_foods[0])
+    # CODE BELOW THIS POINT IS FOR SPRINT 2: SCRAPPING NUTRITION FACTS
+
+    # for i in range(len(split_foods)):
+    #     link = WebDriverWait(driver, timeout=3).until(
+    #         lambda d: d.find_element(By.LINK_TEXT, split_foods[i]['food_name']))
+    #     link.click()
+    #     singleNutritionFact(split_foods, i)
+    #     driver.back()
+
+#Converts the list of dictionaries for split_foods into a JSON file based on the dining hall
+def convertToJSON(split_foods, dining_hall):
+    if dining_hall == 'College Nine/John R. Lewis Dining Hall':
+        file_name = 'college_nine_john_lewis_dining.json'
+    elif dining_hall == 'Cowell/Stevenson Dining Hall':
+        file_name = 'cowell_stevenson_dining.json'
+    elif dining_hall == 'Crown/Merrill Dining Hall':
+        file_name = 'crown_merrill_dining.json'
+    else:
+        file_name = 'porter_kresge_dining.json'
+
+    ofstrm = open(file_name, 'w')
+    ofstrm.write('[\n')
+    for i in split_foods:
+        json.dump(i, ofstrm, indent=4)
+        ofstrm.write(",\n")
+    ofstrm.write('\n]')
+    ofstrm.close()
 
 
 if __name__ == '__main__':
-    dining_halls = ['College Nine/John R. Lewis Dining Hall','Cowell/Stevenson Dining Hall','Crown/Merrill Dining Hall','Porter/Kresge Dining Hall']
+    dining_halls = ['College Nine/John R. Lewis Dining Hall', 'Cowell/Stevenson Dining Hall',
+                    'Crown/Merrill Dining Hall', 'Porter/Kresge Dining Hall']
 
     for i in dining_halls:
         moveToNutritionPage(i)
         getNutritionFacts(i)
-    #singleNutritionFact()
-    driver.get("https://nutrition.sa.ucsc.edu/")
-    time.sleep(1)
+
+    # driver.get("https://nutrition.sa.ucsc.edu/")
+    # time.sleep(1)
     driver.quit()
